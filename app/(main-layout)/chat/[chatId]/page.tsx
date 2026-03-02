496a8984169ac1970bf5812f154361aa8ba48234
Krzysztof <krzysztofjustgdynia@gmail.com>
2026-03-02 19:26:05 +0100

    simple chat layout

--- app/(main-layout)/chat/[chatId]/page.tsx
+++ app/(main-layout)/chat/[chatId]/page.tsx
@@ -1,4 +1,6 @@
+import { ChatInput } from '@/modules/Chat/components/ChatInput/ChatInput'
 import { ChatParams } from './types'
+import { ChatMessage } from '@/modules/Chat/components/ChatMessage/ChatMessage'
 
 export default async function Page({
   params,
@@ -7,5 +9,13 @@ export default async function Page({
 }) {
   const { chatId } = await params
 
-  return <div>Chat ID: {chatId}</div>
+  return (
+    <>
+      <div>Chat ID: {chatId}</div>
+      <div className="w-full flex flex-col items-center">
+        <ChatMessage />
+        <ChatInput />
+      </div>
+    </>
+  )
 }
