b3db513bcf501dfd7e52a33f8ddf37c353a9c84b
Krzysztof <krzysztofjustgdynia@gmail.com>
2026-03-03 10:13:20 +0100

    caht input

--- app/modules/Chat/components/ChatInput/ChatInput.tsx
+++ app/modules/Chat/components/ChatInput/ChatInput.tsx
@@ -1,7 +1,16 @@
+'use client'
 export const ChatInput = () => {
   return (
     <>
-      <div>chatinput</div>
+      <div>
+        <input type="text" className="border border-white"></input>
+        <button
+          onClick={() => {}}
+          className="cursor-pointer border border-amber-50 rounded-sm bg-amber-400 text-black"
+        >
+          Send
+        </button>
+      </div>
     </>
   )
 }
